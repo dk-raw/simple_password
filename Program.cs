f8ef8d9436a0a90c44b468b09342ed1c352d60bb
dk.raw <hello@dkatsios.tk>
2021-02-27 11:27:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aspdotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.Name = "John Doe";
+            s1.Score = 8;
+            Console.WriteLine("type the password");
+            string input = Console.ReadLine();
+            if (s1.check(input))
+            {
+                Console.WriteLine("your in");
+            }
+            else
+            {
+                Console.WriteLine("wrong password");
+            }
+        }
+    }
+
+    class Student
+    {
+        public string Name;
+        public int Score;
+        private string passcode = ".6`3pa/$n9e]r=B[RqUq";
+        public bool check(string pass)
+        {
+            return passcode == pass;
+        }
+    }
+}
